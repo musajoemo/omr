7cd4dac33b719b13157ee6e8ae550c8845e06b3e
musajoemo <john.g.moore.jr@gmail.com>
2014-03-06 13:57:57 -0500

    Update production.rb
    Update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,9 +69,9 @@ Omr::Application.configure do
    config.paperclip_defaults = {
      :storage => :s3,
      :s3_credentials => {
-      :bucket => "onemonthrailsind",
-      :access_key_id => "AKIAI26GBI6GLQT422DQ",
-      :secret_access_key => "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"
+      :bucket => "onemonthrailsindd",
+      :access_key_id => "XXXXX",
+      :secret_access_key => "XXXXX"
      }
    }
 end
